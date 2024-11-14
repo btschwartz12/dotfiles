ff83df3681b92ce996c05dd81c9d39a39b35492d
Ben Schwartz <scben@umich.edu>
2024-11-14 15:40:08 -0500

    bruh

--- nvim_install.sh
+++ nvim_install.sh
@@ -1,7 +1,7 @@
 echo "This will overwrite your nvim config. Do you wish to proceed? (y/n)"
 read -r response
 if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]; then
-  cp -r ~/dotfiles/nvim ~/.config/nvim
+  cp -r ~/dotfiles/nvim ~/.config
   echo "nvim config copied from repo to home"
 else
   echo "nvim config not copied"
