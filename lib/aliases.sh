3d56adc47ed0b83b2e4f2eeedfdb5f7cbd06cb68
Ben Schwartz <scben@umich.edu>
2025-03-04 00:34:54 -0500

    update

--- lib/aliases.sh
+++ lib/aliases.sh
@@ -1,9 +1,4 @@
 alias ls='ls -G -F -1'
-alias l='tree --du -h -L 1 -F'
-alias t='tmux attach -t $1'
 alias v='omz reload'
-alias vim="nvim"
 alias tp='btop'
 alias cat="bat"
-# TOOD remove
-# alias dotfiles='git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
